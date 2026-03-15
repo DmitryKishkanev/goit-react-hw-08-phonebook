843a968f573a7bb308ba028777982c4a044d442f
DmitryKishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2026-03-15 21:18:39 +0200

    изменил initialState

--- src/redux/auth/auth-initialState.js
+++ src/redux/auth/auth-initialState.js
@@ -1,6 +1,8 @@
+const token = localStorage.getItem('token');
+
 export const authInitialState = {
   user: { name: null, email: null },
-  token: null,
+  token: token || null,
   // isLoggedIn: null,
   isLoggedIn: !!localStorage.getItem('token'),
   isRefreshing: false,
